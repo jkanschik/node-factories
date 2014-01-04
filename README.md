51e27d18899b92566cd94d422f08b95c6700e120
jkanschik <jens_kanschik@freenet.de>
2014-01-04 12:25:19 +0100

    Use grunt + test coverage

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-node-factories [![Build Status](https://secure.travis-ci.org/jkanschik/node-factories.png)](http://travis-ci.org/jkanschik/node-factories) [![Code Climate](https://codeclimate.com/github/jkanschik/node-factories.png)](https://codeclimate.com/github/jkanschik/node-factories) [![Coverage Status](https://coveralls.io/repos/jkanschik/node-factories/badge.png?branch=master)](https://coveralls.io/r/jkanschik/node-factories)
+node-factories [![Build Status](https://secure.travis-ci.org/jkanschik/node-factories.png)](http://travis-ci.org/jkanschik/node-factories) [![Code Climate](https://codeclimate.com/github/jkanschik/node-factories.png)](https://codeclimate.com/github/jkanschik/node-factories)
 ======
 
 A nodejs module for creating test data
---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: {
+        src: ['gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    watch: {
+      unit: {
+        files: ['lib/**/*.js', 'test/**/*.spec.js'],
+        tasks: ['jshint', 'mochaTest']
+      }
+    },
+    mochaTest: {
+      options: {
+        require: 'test/blanket'
+      },
+      all: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.spec.js']
+      },
+      coverage: {
+        options: {
+          reporter: 'html-cov',
+          quiet: true,
+          captureFile: 'coverage.html'
+        },
+        src: ['test/**/*.spec.js']
+      },
+      // The travis-cov reporter will fail the tests if the
+      // coverage falls below the threshold configured in package.json
+      'travis-cov': {
+        options: {
+          reporter: 'travis-cov'
+        },
+        src: ['test/**/*.spec.js']
+      }
+    },
+  });
+
+};
